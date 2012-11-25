bffc482029080ea8a71e891eccb612d47db98104
rycm <mr59220@googlemail.com>
2012-11-25 19:09:12 +0100

    json parsing added

--- zon_example.pl
+++ zon_example.pl
@@ -2,12 +2,14 @@
 
 use strict;
 use LWP::UserAgent;
+use JSON;
+binmode(STDOUT, ":utf8");
 
 # configuration
 
 my $api_key = "ENTER YOUR API KEY HERE";
 my $endpoint = "content";
-my %params = (q => 'title:Obama', fields => 'title,subtitle,uri', limit=>'5');
+my %params = (q => 'title:Obama', fields => 'title,subtitle,href', limit=>'10');
 
 # create url including parameters
 
@@ -20,6 +22,17 @@ my $zon = LWP::UserAgent->new();
 $zon->default_header('X-Authorization' => $api_key);
 my $result = $zon->get($url, %params);
 
-# output
+# parse json result and print output to stdout
 
-print $result->content;
+my $json = JSON->new;
+my $data = $json->decode($result->content);
+
+foreach my $item ($$data{'matches'})
+{	
+	foreach (@$item) {		
+		print "title: " . $_->{'title'} . "\n";
+		print "subtitle: " . $_->{'subtitle'} . "\n";
+		print "link: " . $_->{'href'} . "\n\n";
+	}
+}
+
